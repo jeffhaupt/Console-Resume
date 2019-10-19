20aef42c34d7fb21c285a0ad69a7e3e886ef59ca
jefferyhaupt <43371332+jefferyhaupt@users.noreply.github.com>
2019-10-19 07:03:51 -0400

    Added array of object related to previous places of employment

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert("Hiya!");
+const myName = "Jeffery Haupt";
+
+const jobs = [
+  {
+    companyName: "St. Joseph County Public Defenders Office",
+    jobTitle: "Deputy Public Defender",
+    jobDescription: "Represent indigent juvenile clients in court."
+  },
+  {
+    companyName: "Self-Employed",
+    jobTitle: "Web Developer",
+    jobDescription: "Designed and built client websites using WordPress."
+  },
+  {
+    companyName: "St. Joseph County Prosecutors Office",
+    jobTitle: "Deputy Prosecuting Attorney",
+    jobDescription: "Represent State of Indiana in court."
+  }
+];
+
+console.log(`Name: ${myName.toUpperCase()}`);
