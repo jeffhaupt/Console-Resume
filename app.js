7922ecd359b52433bcdb9be38ed5c34aa3297853
jefferyhaupt <43371332+jefferyhaupt@users.noreply.github.com>
2019-10-19 12:11:11 -0400

    finished project by adding the displaySkill function and appropriate logic necessary to correctly display each skill

--- app.js
+++ app.js
@@ -25,30 +25,55 @@ const jobs = [
 const mySkills = [
   { skill: "Javascript", iscool: true },
   { skill: "HTML", iscool: false },
-  { skill: "CSS", iscool: true }
+  { skill: "CSS", iscool: true },
   { skill: "Playing classical harp", iscool: true },
   { skill: "Spanish", iscool: false },
-  { skill: "Public Speaking", iscool: false }
+  { skill: "Public Speaking", iscool: false },
+  { skill: "Lawyer", iscool: false }
 ];
 
+//simply log the myName, career, and myDescription variables 
 console.log(`Name: ${myName.toUpperCase()}`);
 console.log(`Career: ${career}`);
 console.log(`Description: ${myDescription}`);
 
+//my interest section will loop through myInterests arrays and
+//display each interest
 console.log(`\nMy interests: `);
 for (let i = 0; i < myInterests.length; i++) {
   console.log(`${bulletPoint} ${myInterests[i]}`);
 }
 
-console.log(`\nPrevious Experience\n`);
+//previous experience section will loop through jobs array and objects
+//and then call displayPosition for each job and its objects
+console.log(`\nPrevious Experience:\n`);
 for (let i = 0; i < jobs.length; i++) {
   displayPosition(jobs[i].companyName, jobs[i].jobTitle, jobs[i].jobDescription);
+}
+
+//skill section will loop through the mySkills array of objects and
+//call the displaySkill function to determine if the skill is cool or
+//not before printing BAM in front of the skill
+console.log(`\nMy Skills:\n`);
+for (let i = 0; i < mySkills.length; i++) {
+  displaySkill(mySkills[i].skill, mySkills[i].iscool);
 }
 
 /*********************************************************
 ******      FUNCTIONS   **********************************
 *********************************************************/
 
+//when called will display job title, company name, and a brief description of the job
 function displayPosition(company, job, description) {
   console.log(`${bulletPoint} ${job} at ${company} - ${description}`);
+}
+
+//when called will determine if the skill is cool or not and then either
+//return BAM in front of the skill or not
+function displaySkill(skillToDisplay, coolOrNot) {
+  if(coolOrNot) {
+    console.log(`${bulletPoint} BAM: ${skillToDisplay}`);
+  } else {
+    console.log(`${bulletPoint} ${skillToDisplay}`);
+  }
 }
